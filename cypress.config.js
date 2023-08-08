4ea1cdc8108c460a7ab09577357b0ee8681ef2c1
Gopal Nambiar <gopalnambiar2@gmail.com>
2023-08-08 16:37:16 -0500

    Cypress support

---
+++ cypress.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  e2e: {
+    baseUrl: 'https://harbor-test.uclusters.app.uffizzi.com/',   
+  },
+};
--- tests/cypress.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'cypress';
-
-export default defineConfig({
-    e2e: {
-        baseUrl: 'https://harbor-test.uclusters.app.uffizzi.com/',
-    },
-});
