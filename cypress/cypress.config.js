8a988fe504497a2b183c086c88e181db43c51fa2
Gopal Nambiar <gopalnambiar2@gmail.com>
2023-08-18 15:59:33 -0500

    Testkube test-3 config

--- cypress/cypress.config.js
+++ cypress/cypress.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   e2e: {
     baseUrl: 'https://harbor.example.com',   
-    supportFile: false
+    supportFile: false,
+    specPattern: 'cypress/e2e/*.cy.{js,jsx,ts,tsx}',
   },
 };
